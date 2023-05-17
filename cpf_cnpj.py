f6d72ddaf96d49a02552ce429412cc4b22bef744
b.ea_ <beatrizrios2001@hotmail.com>
2023-05-16 21:18:01 -0300

    add new

---
+++ cpf_cnpj.py
@@ -0,0 +1,53 @@
+from validate_docbr import CPF, CNPJ
+
+
+class Documento:
+
+    @staticmethod
+    def cria_documento(documento):
+        if len(documento) == 11:
+            return DocCpf(documento)
+        elif len(documento) == 14:
+            return DocCnpj(documento)
+        else:
+            return ValueError("Quantidade de digitos está incorreta")
+
+
+class DocCpf:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cpf = documento
+        else:
+            raise ValueError("CPF inválido")
+
+    def __str__(self):
+        return self.format()
+
+    @staticmethod
+    def valida(documento):
+        validador = CPF()
+        return validador.validate(documento)
+
+    def format(self):
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+
+
+class DocCnpj:
+    def __init__(self, documento):
+        if self.valida(documento):
+            self.cnpj = documento
+        else:
+            raise ValueError("CNPJ inválido!!")
+
+    def __str__(self):
+        return self.format()
+
+    @staticmethod
+    def valida(documento):
+        mascara = CNPJ()
+        return mascara.validate(documento)
+
+    def format(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
